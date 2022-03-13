1ee54e71dc2a6463fdb09dff48755b6007f5fa44
DhananjayGore <dvgore1997@gmail.com>
2022-03-13 23:03:56 +0530

    [DhananjayGore] sliding window leetcode problme no.3

---
+++ dsa-sliding-window/src/LC3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC3_LongestSubstringWithoutRepeatingCharacters {
+
+	public int lengthOfLongestSubstring(String s) {
+
+		Map<Character, Integer> map = new HashMap<>();
+
+		int left = 0, count = 0;
+		for (int right = 0; right < s.length(); right++) {
+			char c = s.charAt(right);
+
+			if (map.containsKey(c)) // here note the left ptr
+				left = Math.max(left, map.get(c) + 1); // abba,right=3 then left cant be set as left+1 hence max
+			count = Math.max(count, right - left + 1);
+			map.put(c, right);
+		}
+		return count;
+	}
+}
