93beea31764e353680f2191245b82c7ab7ee06ab
DhananjayGore <dvgore1997@gmail.com>
2022-03-14 07:31:52 +0530

    format

--- dsa-backtracking/src/LC78_SubSets.java
+++ dsa-backtracking/src/LC78_SubSets.java
@@ -7,24 +7,25 @@ import java.util.List;
 */
 public class LC78_SubSets {
 
-	 List<List<Integer>> b=new ArrayList<>();
-     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> a=new ArrayList<>();
-        return subset(0,nums,a);
-    }
-    
-public List<List<Integer>> subset(int i,int nums[],List<Integer> a){
-    if(i==nums.length){
-        b.add(new ArrayList<>(a));
-        return b;
-    }
-    a.add(nums[i]);
-   
-    subset(i+1,nums,a);
-    a.remove(a.size()-1);
-   
-    subset(i+1,nums,a);
-    return b;
-    }
-	
+	List<List<Integer>> b = new ArrayList<>();
+
+	public List<List<Integer>> subsets(int[] nums) {
+		List<Integer> a = new ArrayList<>();
+		return subset(0, nums, a);
+	}
+
+	public List<List<Integer>> subset(int i, int nums[], List<Integer> a) {
+		if (i == nums.length) {
+			b.add(new ArrayList<>(a));
+			return b;
+		}
+		a.add(nums[i]);
+
+		subset(i + 1, nums, a);
+		a.remove(a.size() - 1);
+
+		subset(i + 1, nums, a);
+		return b;
+	}
+
 }
