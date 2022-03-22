d2fef815ba844101a8fe73ed34fed6ef012e845d
DhananjayGore <dvgore1997@gmail.com>
2022-03-22 21:44:04 +0530

    coin change 2

---
+++ dsa-dynamic-programming/src/medium/knapsack01/LC518CoinChange2.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package medium.knapsack01;
+
+import java.util.HashMap;
+
+/**
+ * @author dhananjay 
+ * @link  : https://leetcode.com/problems/coin-change-2/
+ * @level : medium
+ */
+public class LC518CoinChange2 {
+
+	public int change(int amount, int[] coins) {
+        HashMap<String, Integer> dp = new HashMap<>();
+      return helper(amount, coins, 0, dp);
+    }
+    
+    private int helper(int amount, int coins[], int currIndex,HashMap<String, Integer> dp){
+        if(amount == 0) return 1;
+        
+        if(currIndex == coins.length) return 0;
+        
+        String key = currIndex+"-"+amount;
+        if(dp.containsKey(key)) return dp.get(key);
+        
+        int incl =0;
+        if(amount >= coins[currIndex]){
+            incl = helper(amount-coins[currIndex], coins, currIndex, dp);
+        }
+        int dont_incl = helper(amount, coins, currIndex+1, dp);
+        dp.put(key, incl+dont_incl);
+        return dp.get(key);
+    }
+}
