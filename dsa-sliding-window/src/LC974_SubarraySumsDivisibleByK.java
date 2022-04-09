5b3183d65d990f347fcc41e99c7521a90233c865
DhananjayGore <dvgore1997@gmail.com>
2022-04-09 17:52:32 +0530

    974. Subarray Sums Divisible by K (medium)

--- dsa-sliding-window/src/LC974_SubarraySumsDivisibleByK.java
+++ dsa-sliding-window/src/LC974_SubarraySumsDivisibleByK.java
@@ -1,13 +1,27 @@
+import java.util.HashMap;
+
 /**
  * 
  */
 
 /**
- * @author dhananjay 
- * @link  :
- * @level : 
+ * @author dhananjay
+ * @link : https://leetcode.com/problems/subarray-sums-divisible-by-k/
+ * @level : medium
  */
 public class LC974_SubarraySumsDivisibleByK {
 
-	
+	public int subarraysDivByK(int[] nums, int k) {
+		int count = 0, sum = 0, remainder = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(remainder, 1);
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			remainder = ((sum % k) + k) % k;
+			if (map.containsKey(remainder))
+				count += map.get(remainder);
+			map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+		}
+		return count;
+	}
 }
